957bd6caafd339252206c798e27f872ec069e251
chenxvhua <minefo@aliyun.com>
2020-03-03 09:38:40 +0800

    feat:first

---
+++ src/components/chapter1/section14.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="inner-container" v-else>
+        <breadcrumb :current-router="$route"></breadcrumb>
+        <div class="segment-wraper"><h1 class="segment-title"></h1><div class="segment-content"></div></div>
+        <div class="segment-wraper"><h1 class="segment-title"></h1><div class="segment-content"></div></div>
+        <div class="segment-wraper"><h1 class="segment-title"></h1><div class="segment-content"></div></div>
+        <div class="segment-wraper"><h1 class="segment-title"></h1><div class="segment-content"></div></div>
+        <div class="segment-wraper"><h1 class="segment-title"></h1><div class="segment-content"></div></div>
+        <div class="segment-wraper"><h1 class="segment-title"></h1><div class="segment-content"></div></div>
+        <div class="segment-wraper"><h1 class="segment-title"></h1><div class="segment-content"></div></div>
+        <div class="segment-wraper"><h1 class="segment-title"></h1><div class="segment-content"></div></div>
+        <div class="segment-wraper"><h1 class="segment-title"></h1><div class="segment-content"></div></div>
+        <div class="segment-wraper"><h1 class="segment-title"></h1><div class="segment-content"></div></div>
+
+        <div class="segment-wraper"><h1>本节总结:</h1><div class="summary">
+            1.目前长沙一般主要有三种获得购房资格的方案:1.不落户两年社保;2.落户后一年社保;3.落户后无社保，两年后也可以。
+            这三种是常见的拥有购房资格的方式。你看哪种方式合适了，主要就是落户、交社保了
+            2.工资一万左右
+            3.
+        </div></div>
+
+    </div>
+</template>
+<script>
+    export default {
+        data() {
+            return {
+
+            }
+        },
+        mounted(){
+
+        },
+        watch: {
+            ...pageCommon.pageCommonWatch.commonWatch
+        },
+        methods : {
+            ...pageCommon.pageCommonMethod.opeMethod,//增删改查方法
+            ...pageCommon.pageCommonMethod.otherMethod, //页面其他方法
+        },
+    }
+</script>
+<style scoped>
+
+</style>
